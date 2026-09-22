c89a537802f2c15cdc479d87b972fd5d56d1a326
kazucheat <lxkazi451@gmail.com>
2026-09-22 13:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ async def generate_jwt_token(uid, password):
     """Generate JWT token"""
     try:
         encoded_password = urllib.parse.quote(password)
-        url = f"https://ob55-jwt-token.vercel.app/token?uid={uid}&password={encoded_password}"
+        url = f"https://jwt-token-gen-kazu.vercel.app/token?uid={uid}&password={encoded_password}"
         
         async with aiohttp.ClientSession() as session:
             async with session.get(url, timeout=24) as response:
